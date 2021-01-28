32346938088d7e76a4b934d2074d8cdb1b906fe3
Erwick20021 <72509765+Erwick20021@users.noreply.github.com>
2021-01-27 21:29:59 -0600

    Update indexTest.c

--- CadenaContiene/indexTest.c
+++ CadenaContiene/indexTest.c
@@ -5,8 +5,4 @@ int main ()
 char cadena1[] = "gentrificacion";
 char cadena2[] = "gente";
 buscar(cadena1, cadena2), 0;
-
-char cadena1[] = "almacenes";
-char cadena2[] = "alma";
-buscar(cadena1, cadena2), 1;
 }
