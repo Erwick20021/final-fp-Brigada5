1b97d4ea693d7ebaee0fd445382721d31ab3db26
Erwick20021 <72509765+Erwick20021@users.noreply.github.com>
2021-01-28 22:43:56 -0600

    Update indexTest.c

--- Autobus/indexTest.c
+++ Autobus/indexTest.c
@@ -1,14 +1,5 @@
-/*
 
-int cambios[][] = {{3, 0},{5, 1},{2, 1},{4, 3},{0, 8}};
-calcularPasajeros(cambios,5), 1
-
-int cambios[][] = {{1, 0},{1, 0},{5, 1},{0, 3},{1, 1}};
-calcularPasajeros(cambios,5), 3
-
-*/
-
-#include "..\index.c"
+#include "..\Autobus.c"
 
 int main()
 {
