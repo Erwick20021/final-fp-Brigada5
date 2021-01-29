438efae315dc9468833ffc4245548e256240c4cf
Erwick20021 <72509765+Erwick20021@users.noreply.github.com>
2021-01-28 18:03:06 -0600

    Update indexTest.c

--- Interseccion/indexTest.c
+++ Interseccion/indexTest.c
@@ -1,3 +1,7 @@
+#include  "../Interseccion.c"
+
+int main ()
+{
 int primero[] = {5,9,2,4,6,0};
 int segundo[] = {4,2,7,8,4,5,0};
 int expResult[] = {5,2,4,0};
@@ -7,3 +11,4 @@ int primero2[] = {22,89,4,10,33};
 int segundo2[] = {1,2,9};
 int expResult2[] = {};
 encontrar(primero2, 5, segundo2, 3, expResult2);
+}
